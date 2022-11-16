1ad46c055497ea300daae44e237b3c07e64e4ba1
Josef <jdhamelink0@gmail.com>
2022-11-16 17:39:33 +0100

    Fix high api call frequency
    It's also a lot faster now

--- src/main.py
+++ src/main.py
@@ -83,10 +83,13 @@ def create_dataframe(artists: list[Artist]) -> pd.DataFrame:
     artist_names = [artist.name for artist in artists]
     df = pd.DataFrame(columns=artist_ids)
     df.index.name = 'song_id'
+    n = len(artists)
 
-    for artist in artists:
+    for i, artist in enumerate(artists):
+        print(f'\rprocessing artist {i+1}/{n}...', end='')
         for song in get_relevant_songs_for_artist(artist, artist_ids):
             df.loc[song.id_, song.artist_ids] = 1
+    print('\n-----------------')
     
     # convert all artist ids in df to names for readability
     # FIXME: sometimes multiple artist id's map to the same artist name
@@ -124,22 +127,21 @@ def get_artists_for_playlist(playlist_id: str) -> list[Artist]:
 def get_relevant_songs_for_artist(artist: Artist, all_artist_ids: list[str]) -> list[Song]:
     """   Finds all relevant songs for a given artist.   """
 
-    print(f'querying songs for "{artist.name}"...')
-
     # cast all_artist_ids to a set for faster lookup
     all_artist_ids = set(all_artist_ids)
 
     # get all albums for the artist
     album_ids: list[str] = [album['id'] for album in sp_artist_albums(artist.id_)['items']]
-    # request all albums at once
-    albums: list[dict] = sp_albums(album_ids)['albums']
+    # split album_ids into chunks of 20, request and return responses as one whole
+    album_ids = [album_ids[i:i+20] for i in range(0, len(album_ids), 20)]
+    albums: list[dict] = []
+    for ids_chunk in album_ids:
+        albums += sp_albums(ids_chunk)['albums']
     # unwrap all songs from the albums
     songs: list[dict] = [track for album in albums for track in album['tracks']['items']]
-    n = len(songs)
 
     relevant_songs: list[str] = []
-    for i, song in enumerate(song):
-        print(f'\rprocessing song {i+1}/{n}...', end='')
+    for _, song in enumerate(songs):
         # get the artists for this song
         artist_ids: list[str] = [artist['id'] for artist in song['artists']]
         # if the song has at least two artists we are interested in, add it to the list as a Song object
@@ -147,7 +149,6 @@ def get_relevant_songs_for_artist(artist: Artist, all_artist_ids: list[str]) -> 
         if len(artist_ids_in_playlist) >= 2:
             song_obj = Song(song['id'], song['name'], artist_ids_in_playlist)
             relevant_songs.append(song_obj)
-    print('\n-----------------')
 
     return relevant_songs
 
@@ -171,6 +172,7 @@ def sp_artist_albums(artist_id: str) -> dict:
 @api_call
 def sp_albums(album_ids: list[str]) -> dict:
     return SP.albums(album_ids)
+
 
 if __name__ == '__main__':
     main()
